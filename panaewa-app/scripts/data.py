bd667a4e2005a1627e52e2855083fea1905198ea
cherryleh <cherryle@hawaii.edu>
2026-02-27 10:16:29 -1000

    Add air quality

--- panaewa-app/scripts/data.py
+++ panaewa-app/scripts/data.py
@@ -13,7 +13,10 @@ from zoneinfo import ZoneInfo
 from dotenv import load_dotenv
 
 load_dotenv()
+
 API_TOKEN = os.getenv("HCDP_API_KEY")
+AIRNOW_TOKEN = os.getenv("AIRNOW_KEY")
+
 data_path = '../data'
 public_path = '../public'
 header = {
@@ -141,6 +144,41 @@ elif spi3 <= 0.5:
 else:
     drought = "No"
 
+
+lat, lon = 19.684, -155.052
+
+
+url = "https://www.airnowapi.org/aq/observation/latLong/current/"
+params = {
+    "format": "application/json",
+    "latitude": lat,
+    "longitude": lon,
+    "distance": 23,         # miles
+    "API_KEY": AIRNOW_TOKEN,
+}
+
+r = requests.get(url, params=params, timeout=30)
+r.raise_for_status()
+airnow_data = r.json()
+pm25_aqis = [row.get("AQI") for row in airnow_data if row.get("ParameterName") == "PM2.5" and row.get("AQI") is not None][0]
+
+if pm25_aqis <= 50:
+    air_quality = "Good"
+elif pm25_aqis <= 100:
+    air_quality = "Moderate"
+elif pm25_aqis <= 150:
+    air_quality = "Unhealthy for Sensitive Groups"
+elif pm25_aqis <= 200:
+    air_quality = "Unhealthy"
+elif pm25_aqis <= 300:
+    air_quality = "Very Unhealthy"
+elif pm25_aqis >= 300:
+    air_quality = "Hazardous"
+else:
+    air_quality = "NA"
+
+
+
 data = {
     "YestYr": yestYr,
     "YestMonth": yestMonth,
@@ -156,7 +194,8 @@ data = {
     "rf_pdiff": round(rf_pdiff*100,0),
     "wind_speed": 10,
     "wind_direction": "NE",
-    "drought": f"{drought}"
+    "drought": f"{drought}",
+    "air_quality": f"{air_quality}"
 }
 
 with open(os.path.join(public_path, "weather_vars.json"), "w") as f_out:
--- panaewa-app/scripts/test.py
+++ panaewa-app/scripts/test.py
@@ -1,86 +1,40 @@
 import requests
-import geopandas as gpd
-import rasterio
-import pandas as pd
-from rasterstats import zonal_stats
-import json
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
 import os
-from zoneinfo import ZoneInfo
-
-
 from dotenv import load_dotenv
 
 load_dotenv()
-API_TOKEN = os.getenv("HCDP_API_KEY")
-data_path = ''
-public_path = '../public'
-header = {
-    "Authorization": f"Bearer {API_TOKEN}",
-    "Content-Type": "application/json"
+
+
+lat, lon = 19.684, -155.052
+
+
+url = "https://www.airnowapi.org/aq/observation/latLong/current/"
+params = {
+    "format": "application/json",
+    "latitude": lat,
+    "longitude": lon,
+    "distance": 23,         # miles
+    "API_KEY": AIRNOW_TOKEN,
 }
 
-print("HCDP_API_KEY loaded:", "yes" if os.getenv("HCDP_API_KEY") else "no")
+r = requests.get(url, params=params, timeout=30)
+r.raise_for_status()
+airnow_data = r.json()
+pm25_aqis = [row.get("AQI") for row in airnow_data if row.get("ParameterName") == "PM2.5" and row.get("AQI") is not None][0]
 
-now_hst = datetime.now(ZoneInfo("Pacific/Honolulu"))
-yesterday_hst = now_hst - relativedelta(days=1)
-
-yestYr, yestMonth, yestDay = yesterday_hst.year, yesterday_hst.month, yesterday_hst.day
-
-last_month_hst = now_hst - relativedelta(months=1)
-lastMonth = last_month_hst.month
-lastMonthYr = last_month_hst.year
-
-
-def get_tif(url, file):
-    print(f"Requesting: {url}")
-    r = requests.get(url, headers=header)
-    print("HTTP status:", r.status_code)
-
-    if r.status_code == 404:
-        print("→ Raster not available, skipping")
-        return False
-
-    r.raise_for_status()
-
-    with open(file, "wb") as f:
-        f.write(r.content)
-
-    print("Saved:", file, "(", os.path.getsize(file), "bytes )")
-    return True
-
-
-
-
-spi_url = f'https://api.hcdp.ikewai.org/raster?date={yestYr}-{lastMonth:02d}&datatype=spi&timescale=timescale003&period=month'
-spi3_file = os.path.join(data_path, "spi3.tif")
-get_tif(spi_url, spi3_file)
-
-ranchshp = gpd.read_file(os.path.join(data_path, "../data/panaewa.shp"))
-rh_file=os.path.join(data_path, "relative_humidity_daily.tif")
-
-def get_zonal_stats(file):
-    with rasterio.open(file) as src:
-        affine = src.transform
-        array = src.read(1)
-        df_zonal_stats = pd.DataFrame(zonal_stats(ranchshp, array, affine=affine,nodata=src.nodata,stats = ['mean']))
-    return df_zonal_stats['mean'].iloc[0]
-
-
-spi3 = get_zonal_stats(spi3_file)
-if spi3 <= -2.0:
-    drought = "Exceptional"
-elif spi3 <= -1.6:
-    drought = "Extreme"
-elif spi3 <= -1.3:
-    drought = "Severe"
-elif spi3 <= -0.8:
-    drought = "Moderate"
-elif spi3 <= 0.5:
-    drought = "Abnormally Dry"
+if pm25_aqis <= 50:
+    air_quality = "Good"
+elif pm25_aqis <= 100:
+    air_quality = "Moderate"
+elif pm25_aqis <= 150:
+    air_quality = "Unhealthy for Sensitive Groups"
+elif pm25_aqis <= 200:
+    air_quality = "Unhealthy"
+elif pm25_aqis <= 300:
+    air_quality = "Very Unhealthy"
+elif pm25_aqis >= 300:
+    air_quality = "Hazardous"
 else:
-    drought = "No"
+    air_quality = "NA"
 
-print(spi3)
-
+print(air_quality)
